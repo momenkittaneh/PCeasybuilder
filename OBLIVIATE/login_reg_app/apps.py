4b172c6a5b882941e44cc7628589ce14af900e92
mutazzahdeh <mutazzahdeh@gmail.com>
2021-06-02 10:46:09 +0300

    project

---
+++ OBLIVIATE/login_reg_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LoginRegAppConfig(AppConfig):
+    name = 'login_reg_app'
---
+++ OBLIVIATE/login_reg_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,HttpResponse,redirect
+
+# Create your views here.
+def index(reqeust):
+    pass
