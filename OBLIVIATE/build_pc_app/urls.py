5d7be91799815d9822e5f9c311d64df7bc6931ec
momenkittaneh <momen.kittaneh93@outlook.com>
2021-06-05 15:56:52 +0300

    "upload"

--- OBLIVIATE/build_pc_app/urls.py
+++ OBLIVIATE/build_pc_app/urls.py
@@ -8,11 +8,6 @@ urlpatterns = [
     path('confirm',views.ok),
     path('cart',views.mycart),
     path('details/<int:id>',views.proddetails),
-<<<<<<< HEAD
-    path('profile',views.myprofile),
-    path('addaddress/<int:id>',views.addaddress),
-=======
     path('order/<int:id>', views.order_view),
->>>>>>> b8396824de2f47a231faf9eb2ca7735bbc0c3cf4
     
 ]
